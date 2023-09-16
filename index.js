eecbb61e22afb34c302f8507d911bdf3fd9e5bab
TaoMaru <jackson_maria1@columbusstate.edu>
2023-09-16 18:48:01 -0400

    Add form with post and redirect to home

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 app.use('/static', express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -17,6 +20,11 @@ app.get('/query', function( req, res ){
     let name = req.query.name;
     let nameObj = {"name": name}
     res.render('home.ejs', nameObj);
+});
+
+app.post("/create", (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
 });
 
 app.listen(3000, () => {
