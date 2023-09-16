c4d9d333bb66e83f1dd0534f8e7bb8b7d9ec60fd
TaoMaru <jackson_maria1@columbusstate.edu>
2023-09-16 18:02:52 -0400

    Add path and query params to take name

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function( req, res ){
-    res.render('home.ejs')
+    res.render('home.ejs', {name: null});
 });
 
-// app.get('/results', function( req, res ){
-//     res.render('results.ejs')
-// });
+app.get('/path/:name', function( req, res ){
+    let name = req.params.name;
+    res.render('home.ejs', {name: name});
+});
 
-// app.get('/contact', function( req, res ){
-//     res.render('contact.ejs')
-// });
+app.get('/query', function( req, res ){
+    let name = req.query.name;
+    let nameObj = {"name": name}
+    res.render('home.ejs', nameObj);
+});
 
 app.listen(3000, () => {
-    console.log('started on port 3000')
+    console.log('started on port 3000');
 });
