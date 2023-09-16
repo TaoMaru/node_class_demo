724e4a8e8e8e077414678ec49ac62028c1591b15
TaoMaru <jackson_maria1@columbusstate.edu>
2023-09-16 15:37:32 -0400

    Add home.ejs, bootstrap template, views and stylesheet

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
 
+app.use('/static', express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', function( req, res ){
-    res.send('Hello, World!')
+    res.render('home.ejs')
 });
+
+// app.get('/results', function( req, res ){
+//     res.render('results.ejs')
+// });
+
+// app.get('/contact', function( req, res ){
+//     res.render('contact.ejs')
+// });
 
 app.listen(3000, () => {
     console.log('started on port 3000')
